6580ff41b7136363915ac20aebdbd9d0a526e827
Yevgniya Skubiy <yevgeniyaskubiy@gmail.com>
2024-05-06 21:37:20 +0300

    06.05

--- js/js_sp.js
+++ js/js_sp.js
@@ -1,3 +1,4 @@
+//CCI
 // Отримуємо кнопку "Більше..." та модальне вікно для програми ССІ за їхніми ідентифікаторами
 var openModalButtonCCI = document.getElementById('open-modal-cci');
 var modalCCI = document.getElementById('modal-cci');
@@ -19,25 +20,39 @@ window.addEventListener('click', function(event) {
         modalCCI.style.display = 'none';
     }
 });
-
-// Отримуємо кнопку "Більше..." та модальне вікно для програми ERASMUS за їхніми ідентифікаторами
+//ERASMUS
 var openModalButtonErasmus = document.getElementById('open-modal-erasmus');
 var modalErasmus = document.getElementById('modal-erasmus');
 
-// Додаємо обробник події для кнопки "Більше..." для програми ERASMUS
 openModalButtonErasmus.addEventListener('click', function() {
     modalErasmus.style.display = 'block'; // Показуємо модальне вікно для програми ERASMUS
 });
 
-// Отримуємо кнопку закриття та додаємо обробник події для закриття модального вікна для програми ERASMUS
 var closeButtonErasmus = modalErasmus.querySelector('.close');
 closeButtonErasmus.addEventListener('click', function() {
     modalErasmus.style.display = 'none'; // Закриваємо модальне вікно для програми ERASMUS
 });
 
-// Додаємо обробник події для закриття модального вікна при кліку поза ним для програми ERASMUS
 window.addEventListener('click', function(event) {
     if (event.target === modalErasmus) {
         modalErasmus.style.display = 'none';
+    }
+});
+//AMERICAN
+var openModalButtonAmerican = document.getElementById('open-modal-amer');
+var modalAmerican = document.getElementById('modal-amer');
+
+openModalButtonAmerican.addEventListener('click', function() {
+    modalAmerican.style.display = 'block'; 
+});
+
+var closeButtonAmerican = modalAmerican.querySelector('.close');
+closeButtonAmerican.addEventListener('click', function() {
+    modalAmerican.style.display = 'none'; 
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === modalAmerican) {
+        modalAmerican.style.display = 'none';
     }
 });
